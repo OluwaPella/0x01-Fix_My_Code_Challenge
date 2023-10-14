11c361b93ee20eddb2acc34eaff55992f31520c3
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-10-14 11:11:41 +0100

    very good

--- status_server/api/v1/views.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from flask import Blueprint, jsonify
-
-app_views = Blueprint('app_views', __name__)
-
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    return jsonify({"status": "API is up and running"})
